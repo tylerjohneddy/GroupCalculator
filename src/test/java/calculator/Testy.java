8a94c911f443d9af0776126615d19b46c2aa0d97
Tyler Eddy <tylereddy96@gmail.com>
2020-02-10 14:19:26 +0000

    added sub tests

--- src/test/java/calculator/Testy.java
+++ src/test/java/calculator/Testy.java
@@ -1,5 +1,19 @@
 package calculator;
 
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Test;
+
 public class Testy {
+	@Test
+	public void subtractTest() {
+		Calculator calc = new Calculator();
+		assertEquals(-5, calc.subtract(10, 15), 0.002);
+		assertEquals(5, calc.subtract(10, 5), 0.002);
+		assertEquals(-105, calc.subtract(10, 115), 0.002);
+		assertEquals(-165, calc.subtract(10, 175), 0.002);
+		assertEquals(-25, calc.subtract(-10, 15), 0.002);
+
+	}
 
 }
